aff5db86a83037f8e832bc115a3b517e10871dd7
minenash <minenash@protonmail.com>
2022-10-27 01:46:22 -0400

    Updated to 1.19

--- src/main/java/com/minenash/nameless_servers/mixin/MultiplayerScreenMixin.java
+++ src/main/java/com/minenash/nameless_servers/mixin/MultiplayerScreenMixin.java
@@ -2,25 +2,23 @@ package com.minenash.nameless_servers.mixin;
 
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
-import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(MultiplayerScreen.class)
 public class MultiplayerScreenMixin {
 
     @Shadow protected MultiplayerServerListWidget serverListWidget;
 
-    @Redirect(method = "method_19914", at = @At(value = "NEW", ordinal = 1, target = "net/minecraft/text/TranslatableText"))
-    private TranslatableText changeText(String key, Object[] old) {
+    @ModifyArg(method = "method_19914", index = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/text/MutableText;"))
+    private Object[] changeText(Object[] old) {
         if (((String)old[0]).isBlank()) {
-            String str = ((MultiplayerServerListWidget.ServerEntry) serverListWidget.getSelected()).getServer().address;
-            return new TranslatableText(key, str);
+            String str = ((MultiplayerServerListWidget.ServerEntry) serverListWidget.getSelectedOrNull()).getServer().address;
+            return new Object[]{str};
         }
-
-        return new TranslatableText(key, old);
+        return old;
     }
 
 }
