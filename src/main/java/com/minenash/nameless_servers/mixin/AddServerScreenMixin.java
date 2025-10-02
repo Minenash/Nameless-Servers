89993ac5e31cef2dbeff2b4556ee3b8f2b1bfc12
Jakob <minenash@protonmail.com>
2025-10-01 23:32:37 -0400

    Updated to 1.21.9

--- src/main/java/com/minenash/nameless_servers/mixin/AddServerScreenMixin.java
+++ src/main/java/com/minenash/nameless_servers/mixin/AddServerScreenMixin.java
@@ -1,16 +1,23 @@
 package com.minenash.nameless_servers.mixin;
 
 import net.minecraft.client.gui.screen.multiplayer.AddServerScreen;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AddServerScreen.class)
 public class AddServerScreenMixin {
 
-    @Redirect(method = "updateAddButton", at = @At(value = "INVOKE", target = "Ljava/lang/String;isEmpty()Z"))
-    private boolean allowEmptyNames(String _string) {
-        return false;
+    @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setPlaceholder(Lnet/minecraft/text/Text;)V"))
+    public Text removePlaceholder(Text _placeholder) {
+        return Text.empty();
+    }
+
+    @Redirect(method = "addAndClose", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;getString()Ljava/lang/String;"))
+    public String keepServerNameBlank(Text _text) {
+        return "";
     }
 
 }
